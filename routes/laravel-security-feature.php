c56bcd4229f5ef5f56151bccd359abeb33dcd446
Prajwol <prazzolgautam@gmail.com>
2025-10-10 17:16:47 +0545

    changes in email verified middleware

--- routes/laravel-security-feature.php
+++ routes/laravel-security-feature.php
@@ -6,6 +6,6 @@ use App\Http\Controllers\Auth\LaravelSecurityFeatureController;
 Route::post('/laravel-security-feature/verify', [LaravelSecurityFeatureController::class, 'verify'])
     ->name('laravel-security-feature.verify');
 
-Route::post('/laravel-security-feature/changeEmailAndSendOtp',[LaravelSecurityFeatureController::class, 'changeEmailAndSendOtp'])
+Route::post('/laravel-security-feature/changeEmailAndSendOtp/{user_id}',[LaravelSecurityFeatureController::class, 'changeEmailAndSendOtp'])
     ->name('laravel-security-feature.changeEmailAndSendOtp');
 
