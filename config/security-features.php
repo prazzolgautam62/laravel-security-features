3ce2991d117bf8023d8faad37789649e08f48804
Prajwol <prazzolgautam@gmail.com>
2025-10-14 13:28:43 +0545

    changes

--- config/security-features.php
+++ config/security-features.php
@@ -9,6 +9,7 @@ return [
     'verification_code_expiry' => 10, // Minutes
     'device_identifier' => 'user_agent', // Or 'user_agent_ip' for stricter (but IP changes often)
     'email_from' => env('MAIL_FROM_ADDRESS', 'no-reply@laravelsecurity.com'),
+    'email_from_name' => env('MAIL_FROM_NAME', 'no-reply@laravelsecurity.com'),
     '2fa_validity_days' => env('SECURITY_2FA_VALIDITY_DAYS',30),
     'otp_length' => env('SECURITY_OTP_LENGTH',6),
     'superadmin_email_to' => env('SECURITY_SUPERADMIN_EMAIL_TO','superadmin@laravelsecurity.com')
